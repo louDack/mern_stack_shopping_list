da04c81382951690c62291e3a7e35134b01d02b7
Deric Siu <dddsiu@derics-mbp.lan>
2021-08-26 23:06:18 -0700

    Added Reducer for getItems.

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,18 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import AppNavBar from './components/AppNavBar';
 import ShoppingList from './components/ShoppingList';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AppNavBar />
-        <ShoppingList />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <AppNavBar />
+          <ShoppingList />
+        </div>
+      </Provider>
     )
   }
 }
