e1b6f09304a12415164409266739217d8880b579
Deric Siu <dddsiu@derics-mbp.lan>
2021-08-27 20:39:18 -0700

    ItemModal component works now.

--- client/src/App.js
+++ client/src/App.js
@@ -7,13 +7,19 @@ import ShoppingList from './components/ShoppingList';
 import { Provider } from 'react-redux';
 import store from './store';
 
+import ItemModal from './components/ItemModal'
+import { Container } from 'reactstrap'
+
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <div className="App">
           <AppNavBar />
-          <ShoppingList />
+          <Container>
+            <ItemModal />
+            <ShoppingList />
+          </Container>
         </div>
       </Provider>
     )
