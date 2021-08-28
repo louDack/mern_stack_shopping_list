af4eae5b2b929ea721c423d52db8de67efeb6f12
Deric Siu <dddsiu@derics-mbp.lan>
2021-08-27 19:35:38 -0700

    Removed "Add Button"

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import { v4 as uuidv4 } from 'uuid';
+
 import { connect } from 'react-redux';
 import { getItems, deleteItem } from '../actions/itemActions';
 import PropTypes from 'prop-types';
@@ -19,21 +19,6 @@ class ShoppingList extends Component {
     const { items } = this.props.item;
     return (
       <Container>
-        <Button
-          color="dark"
-          style={{ marginBottom: '2rem' }}
-          onClick={() => {
-            const name = prompt('Enter Item');
-            if (name) {
-              this.setState(state => ({
-                items: [...state.items, { id: uuidv4(), name }]
-              }));
-            }
-          }}
-        >
-          Add Item
-        </Button>
-
         <ListGroup>
           <TransitionGroup className="shopping-list">
             {items.map(({ id, name }) => (
