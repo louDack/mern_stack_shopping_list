e54938bc1cc951fba65f2e141a64f147cb48daa8
Deric Siu <dddsiu@derics-mbp.lan>
2021-08-28 13:57:17 -0700

    Added ability to fetch items from backend and display it.

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -1,9 +1,21 @@
 import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from './types';
+import axios from 'axios'
 
-export const getItems = () => {
-  return {
-    type: GET_ITEMS
-  };
+// if I use '/api/items', shoppingList component doesnt render and I get a 
+// Proxy error: Could not proxy request /api/items from localhost:3000 to http://localhost:5000. See https://nodejs.org/api/errors.html#errors_common_system_errors for more information (ECONNREFUSED).
+
+// AND NOW, axios.get('/api/items') WORKS!!!! for some reason...
+
+export const getItems = () => dispatch => {
+  dispatch(setItemsLoading());
+  axios
+    .get('/api/items')
+    .then(res =>
+      dispatch({
+        type: GET_ITEMS,
+        payload: res.data
+      })
+    )
 };
 
 export const deleteItem = (id) => {
